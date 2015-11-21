29360efc8332067783d40f704536274080bd5646
Leonardo <leonardocolodette0@gmail.com>
2015-11-21 09:09:26 -0200

    Update Livro.php

--- pdoExample/Livro.php
+++ pdoExample/Livro.php
@@ -4,19 +4,16 @@ include 'conec.php';
 class Livro{
  
  function insetLivro($nomeLivro,$autorLivro,$paginasLivro,$estadoLivro){
+  
+  $insertLivro = $pdo->prepare("insert into livro (nomeLivro,autorLivro,paginasLivro,estadoLivro) values
+  (:nomeLivro,:autorLivro,:paginasLivro,:estadoLivro)");
+   
+   $insertLivro->bindValue(":nomeLivro",$nomeLivro);
+   $insertLivro->bindValue(":autorLivro",$autorLivro);
+   $insertLivro->bindValue(":paginasLivro",$paginasLivro);
+   $insertLivro->bindValue(":estadoLivro",$estadoLivro);
     
-    $insertLivro = $pdo->prepare("insert into livro (nomeLivro,autorLivro,paginasLivro,estadoLivro) values
-    (:nomeLivro,:autorLivro,:paginasLivro,:estadoLivro)");
-
-      $insertLivro->bindValue(":nomeLivro",$nomeLivro);
-      $insertLivro->bindValue(":autorLivro",$autorLivro);
-      $insertLivro->bindValue(":paginasLivro",$paginasLivro);
-      $insertLivro->bindValue(":estadoLivro",$estadoLivro);
-    
-     
  }
-    
-
 }
 
- ?>
+?>
